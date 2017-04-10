1e6af09b5eab83b0300b4dc4e406284ccedb2437
Sudhanva Narayana <nsudhanva@gmail.com>
2017-04-10 10:37:59 +0530

    Fill boxes hall

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,10 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
   def index
+  	@total_flats = Resident.all.count
+
+  	@owners = Resident.where("resident_type_id = ?", 1).count
+  	@tenants = Resident.where("resident_type_id = ?", 2).count
+  	@total_residents = Resident.sum(:family)
   end
 end
